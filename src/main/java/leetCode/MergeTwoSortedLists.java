55392df0e8b23a9d9adc13299726e1e68d1d2d36
Vaynshteyn <89127911+Shatey@users.noreply.github.com>
2022-08-30 16:14:54 +0300

    MergeTwoSortedLists solved

--- src/main/java/leetCode/MergeTwoSortedLists.java
+++ src/main/java/leetCode/MergeTwoSortedLists.java
@@ -1,4 +1,44 @@
 package leetCode;
 
 public class MergeTwoSortedLists {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = null;
+        if (list1 != null || list2 != null) {
+            if (list1 == null) {
+                res = list2;
+                list2 = list2.next;
+            } else if (list2 == null) {
+                res = list1;
+                list1 = list1.next;
+            } else {
+                if (list1.val <= list2.val) {
+                    res = list1;
+                    list1 = list1.next;
+                } else {
+                    res = list2;
+                    list2 = list2.next;
+                }
+            }
+            res.next = mergeTwoLists(list1, list2);
+        }
+        return res;
+    }
+
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+
+        ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
--- src/test/java/leetCodeTest/MergeTwoSortedListsTest.java
+++ src/test/java/leetCodeTest/MergeTwoSortedListsTest.java
@@ -1,4 +1,31 @@
 package leetCodeTest;
 
+import leetCode.MergeTwoSortedLists;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MergeTwoSortedListsTest {
+
+    @Test
+    void test() {
+        var first3 = new MergeTwoSortedLists.ListNode(4);
+        var first2 = new MergeTwoSortedLists.ListNode(2, first3);
+        var first1 = new MergeTwoSortedLists.ListNode(1, first2);
+        var second3 = new MergeTwoSortedLists.ListNode(4);
+        var second2 = new MergeTwoSortedLists.ListNode(3, second3);
+        var second1 = new MergeTwoSortedLists.ListNode(1, second2);
+        var target6 = new MergeTwoSortedLists.ListNode(4);
+        var target5 = new MergeTwoSortedLists.ListNode(4, target6);
+        var target4 = new MergeTwoSortedLists.ListNode(3, target5);
+        var target3 = new MergeTwoSortedLists.ListNode(2, target4);
+        var target2 = new MergeTwoSortedLists.ListNode(1, target3);
+        var target1 = new MergeTwoSortedLists.ListNode(1, target2);
+        var res = MergeTwoSortedLists.mergeTwoLists(first1, second1);
+        while (target1.next != null && res.next != null) {
+            assertEquals(target1.val, res.val);
+            target1 = target1.next;
+            res = res.next;
+        }
+    }
 }
